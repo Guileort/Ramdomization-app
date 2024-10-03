05dc1a54adb8243ebf26470f17b46ad877f35027
Guileort <52334705+Guileort@users.noreply.github.com>
2024-10-03 16:26:29 -0500

    Random.py

--- Random.py
+++ Random.py
@@ -1,31 +1,31 @@
-
 import pandas as pd
 import streamlit as st
 import numpy as np
-from time import sleep 
-
+from time import sleep
 
 def form_groups_from_excel(df):
     # Combine the first and last names into a full name
     df['Full Name'] = df['First Name'] + ' ' + df['Last Name']
 
-    # Separate names based on the score
-    #ID ==Score
-    high_score_names = df[df['ID'] >= 3.0]['Full Name'].tolist()
-    low_score_names = df[df['ID'] < 3.0]['Full Name'].tolist()
+    # Separate names based on the score into three categories: high, medium, low
+    high_score_names = df[df['ID'] >= 3.8]['Full Name'].tolist()
+    medium_score_names = df[(df['ID'] >= 3.2) & (df['ID'] < 3.8)]['Full Name'].tolist()
+    low_score_names = df[df['ID'] < 3.2]['Full Name'].tolist()
 
     # Shuffle the lists to randomize the groups
     np.random.shuffle(high_score_names)
+    np.random.shuffle(medium_score_names)
     np.random.shuffle(low_score_names)
 
-
-# If high_score_names has an odd number of names, move one to low_score_names
+    # If high_score_names has an odd number of names, move one to medium_score_names
     if len(high_score_names) % 2 != 0:
-        low_score_names.append(high_score_names.pop())
+        medium_score_names.append(high_score_names.pop())
 
+    # If medium_score_names has an odd number of names, move one to low_score_names
+    if len(medium_score_names) % 2 != 0:
+        low_score_names.append(medium_score_names.pop())
 
-
-    # Function to create groups
+    # Function to create groups of 2 or 3
     def create_groups(names):
         groups = []
         i = 0
@@ -38,18 +38,18 @@ def form_groups_from_excel(df):
                 i += 2
         return groups
 
-    # Create groups for high and low scores separately
+    # Create groups for high, medium, and low scores separately
     high_score_groups = create_groups(high_score_names)
+    medium_score_groups = create_groups(medium_score_names)
     low_score_groups = create_groups(low_score_names)
 
-    return high_score_groups + low_score_groups
+    return high_score_groups + medium_score_groups + low_score_groups
 
 # Streamlit app
 ### Position logo on right side
 left_co, cent_co,last_co = st.columns(3)
 with last_co:
     st.image("https://raw.githubusercontent.com/Guileort/Ramdomization-app/f4555721973318b15276a5276830b5400109d175/logo.svg?raw=true", width=100)
-
 
 new_title = '<p style="font-family:sans-serif; color:rgb(0, 0, 139); font-size: 32px; font-weight: bold"> Aleatoreitor APP</p>'
 st.markdown(new_title, unsafe_allow_html=True)
@@ -78,7 +78,6 @@ st.markdown("""
 # File uploader
 uploaded_file = st.file_uploader("Seleccione un archivo de excel", type=["xlsx"])
 
-
 # Button to repeat randomization
 randomize_button = st.button("Aleatorizar")
 
@@ -100,4 +99,3 @@ if uploaded_file is not None and randomize_button:
         st.write(group_text)
         group_data.append(group_text)
         sleep(2)
-    
