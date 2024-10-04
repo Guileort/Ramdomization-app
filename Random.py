4beb398e21d330b6db026c72a091b17febac05a8
Guileort <52334705+Guileort@users.noreply.github.com>
2024-10-04 14:31:11 -0500

    Random.py

--- Random.py
+++ Random.py
@@ -93,8 +93,8 @@ if uploaded_file is not None and randomize_button:
     
     for idx, group in enumerate(groups, start=1):
         if len(group) == 3:
-            group_text = f"**Grupo {idx}**: {group[0]}, {group[1]}, {group[2]}"
+            group_text = f"**Grupo {idx}**: {group[0]}, {group[1]}  y  {group[2]}"
         else:
-            group_text = f"**Grupo {idx}**: {group[0]} y {group[1]}"
+            group_text = f"**Grupo {idx}**: {group[0]}  y  {group[1]}"
         st.markdown(f"{group_text}",  unsafe_allow_html=True)
         sleep(2)
